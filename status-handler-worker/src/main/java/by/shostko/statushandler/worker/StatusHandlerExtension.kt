b6c5c69eabcc79f0a6ba49223c54284053a11e2c
Sergey Shostko <s.shostko@gmail.com>
2019-10-14 14:46:50 +0300

    add prepare functions to Worker module

--- status-handler-worker/src/main/java/by/shostko/statushandler/worker/StatusHandlerExtension.kt
+++ status-handler-worker/src/main/java/by/shostko/statushandler/worker/StatusHandlerExtension.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package by.shostko.statushandler.worker
 
 import android.os.Handler
@@ -13,25 +15,38 @@ import io.reactivex.FlowableTransformer
 import io.reactivex.functions.BiFunction
 import java.util.*
 
+private fun <E> StatusHandler<E>.startWorkAndObserveStatus(lifecycleOwner: LifecycleOwner, callable: () -> UUID) {
+    val uuid = callable.invoke()
+    Handler(Looper.getMainLooper()).post {
+        WorkManager.getInstance().getWorkInfoByIdLiveData(uuid)
+            .observe(lifecycleOwner, Observer { info ->
+                info?.state?.let {
+                    when {
+                        !it.isFinished -> updateWorking()
+                        it == WorkInfo.State.FAILED -> updateFailed(info.outputData.keyValueMap)
+                        else -> updateSuccess()
+                    }
+                }
+            })
+    }
+}
+
 fun <E, T> StatusHandler<E>.combineWithWorker(lifecycleOwner: LifecycleOwner, callable: () -> UUID): FlowableTransformer<T, T> =
     FlowableTransformer { upstream ->
         Flowable.combineLatest(
             action.startWith(Action.REFRESH)
-                .doOnNext {
-                    val uuid = callable.invoke()
-                    Handler(Looper.getMainLooper()).post {
-                        WorkManager.getInstance().getWorkInfoByIdLiveData(uuid)
-                            .observe(lifecycleOwner, Observer { info ->
-                                info?.state?.let {
-                                    when {
-                                        !it.isFinished -> updateWorking()
-                                        it == WorkInfo.State.FAILED -> updateFailed(info.outputData.keyValueMap)
-                                        else -> updateSuccess()
-                                    }
-                                }
-                            })
-                    }
-                }
+                .doOnNext { startWorkAndObserveStatus(lifecycleOwner, callable) }
+                .ignoreElements()
+                .toFlowable<Unit>()
+                .startWith(Unit)
+                .distinctUntilChanged(),
+            upstream, BiFunction { _, t -> t })
+    }
+
+fun <E, T> StatusHandler<E>.prepareWithWorker(lifecycleOwner: LifecycleOwner, callable: () -> UUID): FlowableTransformer<T, T> =
+    FlowableTransformer { upstream ->
+        Flowable.combineLatest(
+            action.doOnNext { startWorkAndObserveStatus(lifecycleOwner, callable) }
                 .ignoreElements()
                 .toFlowable<Unit>()
                 .startWith(Unit)
