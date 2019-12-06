e1b385f4aba86b3a92abc255e5aaada1673b2efc
Sergey Shostko <s.shostko@gmail.com>
2019-12-06 18:13:59 +0300

    extend correct ViewModel in SimpleMessage and SimpleCalss

--- status-handler-viewmodel/src/main/java/by/shostko/statushandler/viewmodel/ProcessingViewModel.kt
+++ status-handler-viewmodel/src/main/java/by/shostko/statushandler/viewmodel/ProcessingViewModel.kt
@@ -5,8 +5,8 @@ package by.shostko.statushandler.viewmodel
 import by.shostko.statushandler.*
 
 abstract class SimpleViewModel : ProcessingViewModel<Unit>(Unit, SimpleStatusFactory()) {
-    abstract class Message : CustomViewModel<String>("", MessageStatus.Factory())
-    abstract class Class : CustomViewModel<java.lang.Class<out Throwable>>(NoErrorThrowable::class.java, ClassStatus.Factory())
+    abstract class Message : ProcessingViewModel<String>("", MessageStatus.Factory())
+    abstract class Class : ProcessingViewModel<java.lang.Class<out Throwable>>(NoErrorThrowable::class.java, ClassStatus.Factory())
 }
 
 abstract class ProcessingViewModel<E>(noError: E, factory: Status.Factory<E>?) : CustomViewModel<E>(noError, factory) {
