c261ac038191ee32c93fdce16921b0c26d4845a6
Sergey Shostko <siarhei.shostka@instinctools.com>
2021-03-17 18:31:27 +0300

    make paging2 extenstions internal

--- status-handler-paging2/src/main/java/by/shostko/statushandler/paging/DataSourceExt.kt
+++ status-handler-paging2/src/main/java/by/shostko/statushandler/paging/DataSourceExt.kt
@@ -4,20 +4,20 @@ import androidx.paging.ItemKeyedDataSource
 import androidx.paging.PageKeyedDataSource
 import androidx.paging.PositionalDataSource
 
-fun <K> ItemKeyedDataSource.LoadInitialParams<K>.asString() =
+internal fun <K> ItemKeyedDataSource.LoadInitialParams<K>.asString() =
     "LoadInitialParams(requestedInitialKey=$requestedInitialKey, requestedLoadSize=$requestedLoadSize, placeholdersEnabled=$placeholdersEnabled)"
 
-fun <K> ItemKeyedDataSource.LoadParams<K>.asString() =
+internal fun <K> ItemKeyedDataSource.LoadParams<K>.asString() =
     "LoadParams(key=$key, requestedLoadSize=$requestedLoadSize)"
 
-fun <K> PageKeyedDataSource.LoadInitialParams<K>.asString() =
+internal fun <K> PageKeyedDataSource.LoadInitialParams<K>.asString() =
     "LoadInitialParams(requestedLoadSize=$requestedLoadSize, placeholdersEnabled=$placeholdersEnabled)"
 
-fun <K> PageKeyedDataSource.LoadParams<K>.asString() =
+internal fun <K> PageKeyedDataSource.LoadParams<K>.asString() =
     "LoadParams(key=$key, requestedLoadSize=$requestedLoadSize)"
 
-fun PositionalDataSource.LoadInitialParams.asString() =
+internal fun PositionalDataSource.LoadInitialParams.asString() =
     "LoadInitialParams(requestedStartPosition=$requestedStartPosition, requestedLoadSize=$requestedLoadSize, pageSize=$pageSize, placeholdersEnabled=$placeholdersEnabled)"
 
-fun PositionalDataSource.LoadRangeParams.asString() =
+internal fun PositionalDataSource.LoadRangeParams.asString() =
     "LoadRangeParams(startPosition=$startPosition, loadSize=$loadSize)"
